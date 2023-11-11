66d1b34124decf7b041e2a149e36e1e1e75416fd
DigitaTransforma <130008366+DigitaTransforma@users.noreply.github.com>
2023-11-11 01:58:00 +0100

    mvp

--- fab.py
+++ fab.py
@@ -8,9 +8,9 @@ load_dotenv(find_dotenv())
 HUGGINGFACEHUB_API_TOKEN = os.environ["HUGGINGFACEHUB_API_TOKEN"]
 
 # Load the Mistral LLM model from the HuggingFaceHub
-repo_id = "HuggingFaceH4/zephyr-7b-beta" #"mistralai/Mistral-7B-Instruct-v0.1"
+repo_id = "mistralai/Mistral-7B-Instruct-v0.1"  # "HuggingFaceH4/zephyr-7b-beta"
 mistral_llm = HuggingFaceHub(
-    repo_id=repo_id, model_kwargs={"temperature": 0.1, "max_new_tokens": 500}
+    repo_id=repo_id, model_kwargs={"temperature": 0.1, "max_new_tokens": 512}
 )
 
 # Define the Prompt Template for FAB framework with an instruction
@@ -29,22 +29,26 @@ prompt = PromptTemplate(template=template, input_variables=["product", "audience
 st.title("FAB Framework Generator")
 
 # User input: Text field for the product/business and text area for context
-product_input = st.text_input("Enter the product/business:")
-audience_input = st.text_area("Define your audience?:")
+product_input = st.text_input("Define your Business or Product:")
+audience_input = st.text_area("Define your Audience:")
 
 # Button to trigger FAB generation
 if st.button("Generate FAB"):
-    # Display a spinner while generating the FAB statements
-    with st.spinner("Generating FAB statements..."):
-        # Create a dictionary with user inputs
-        user_input_dict = {"product": product_input, "audience": audience_input}
+    # Check if required fields are empty
+    if not product_input or not audience_input:
+        st.error("Please fill in both the product/business and audience fields.")
+    else:
+        # Display a spinner while generating the FAB statements
+        with st.spinner("Generating FAB statements..."):
+            # Create a dictionary with user inputs
+            user_input_dict = {"product": product_input, "audience": audience_input}
 
-        # Create an LLMChain for FAB generation using the prompt template
-        llm_chain = LLMChain(prompt=prompt, llm=mistral_llm)
+            # Create an LLMChain for FAB generation using the prompt template
+            llm_chain = LLMChain(prompt=prompt, llm=mistral_llm)
 
-        # Run the LLMChain for FAB generation
-        fab_response = llm_chain.run(user_input_dict)
+            # Run the LLMChain for FAB generation
+            fab_response = llm_chain.run(user_input_dict)
 
-    # Display the FAB statements after the spinner
-    st.write("FAB Statements:")
-    st.write(fab_response)
+        # Display the FAB statements after the spinner
+        st.write("Here are 10 FAB Statements that resonates with your audience:")
+        st.write(fab_response)
